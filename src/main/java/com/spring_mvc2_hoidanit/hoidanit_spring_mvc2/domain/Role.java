bf18f488316a889648b57e0b43645e15d2e3a261
helloMinh <phandinhminh48@gmail.com>
2025-03-02 14:03:51 +0700

    090

--- src/main/java/com/spring_mvc2_hoidanit/hoidanit_spring_mvc2/domain/Role.java
+++ src/main/java/com/spring_mvc2_hoidanit/hoidanit_spring_mvc2/domain/Role.java
@@ -1,9 +1,12 @@
 package com.spring_mvc2_hoidanit.hoidanit_spring_mvc2.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +18,10 @@ public class Role {
 
     private String name;
     private String description;
+
+    // User 
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 
     public Long getId() {
         return id;
--- src/main/java/com/spring_mvc2_hoidanit/hoidanit_spring_mvc2/domain/User.java
+++ src/main/java/com/spring_mvc2_hoidanit/hoidanit_spring_mvc2/domain/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +22,10 @@ public class User {
     private String phone;
     private String avatar;
 
+    // Role 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 
     public Long getId() {
         return id;
